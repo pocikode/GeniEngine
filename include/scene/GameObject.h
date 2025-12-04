54c21a3db23e717c111f34b2c06ddd930b21e8b6
Agus Supriyatna <agus@motionpay.id>
2025-12-04 16:03:10 +0700

    Scene and Game Object

---
+++ include/scene/GameObject.h
@@ -0,0 +1,32 @@
+#pragma once
+#include <memory>
+#include <string>
+#include <vector>
+
+namespace Orbis
+{
+
+class GameObject
+{
+  public:
+    virtual ~GameObject() = default;
+    virtual void Update(float deltaTime);
+    const std::string &GetName() const;
+    void SetName(const std::string &name);
+    GameObject *GetParent();
+    bool IsAlive() const;
+    void MarkForDestroy();
+
+  protected:
+    GameObject() = default;
+
+  private:
+    std::string m_name;
+    GameObject *m_parent = nullptr;
+    std::vector<std::unique_ptr<GameObject>> m_children;
+    bool m_isAlive = true;
+
+    friend class Scene;
+};
+
+} // namespace Orbis
---
+++ include/scene/Scene.h
@@ -0,0 +1,36 @@
+#pragma once
+#include "scene/GameObject.h"
+#include <memory>
+#include <string>
+#include <type_traits>
+#include <vector>
+
+namespace Orbis
+{
+
+class GameObject;
+
+class Scene
+{
+  public:
+    void Update(float deltaTime);
+    void Clear();
+    bool SetParent(GameObject *obj, GameObject *parent);
+
+    GameObject *CreateObject(const std::string &name, GameObject *parent = nullptr);
+
+    template <typename T, typename = typename std::enable_if_t<std::is_base_of_v<GameObject, T>>>
+    T *CreateObject(const std::string &name, GameObject *parent = nullptr)
+    {
+        auto obj = new GameObject();
+        obj->SetName(name);
+        SetParent(obj, parent);
+
+        return obj;
+    }
+
+  private:
+    std::vector<std::unique_ptr<GameObject>> m_objects;
+};
+
+} // namespace Orbis
